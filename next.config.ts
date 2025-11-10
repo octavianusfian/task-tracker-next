9bb36cd290661a17a4c82ce33f8f58166c2c8cc7
Octavianus <octavianus.fian@gmail.com>
2025-11-10 07:48:32 +0700

    change enginetype to client

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // helps Next's server components tracer include prisma packages
+    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
+  },
+  outputFileTracingIncludes: {
+    // include engines for route handlers that use Prisma
+    "app/api/**/route": [
+      "./node_modules/.prisma/client",
+      "./node_modules/@prisma/client",
+    ],
+    // include engines for pages that use Prisma directly
+    "app/**/page": [
+      "./node_modules/.prisma/client",
+      "./node_modules/@prisma/client",
+    ],
+  },
 };
 
 export default nextConfig;
